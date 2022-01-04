181fd392c3b9773d09527dbb1b0c2cfdd24a07a8
coolwhite520 <81929154@qq.com>
2022-01-04 15:20:55 +0800

    3。3。2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yarn build
 #read -p "Enter Version:" version
-docker build -t web:3.3.1 .
-docker save -o web.tar web:3.3.1
-docker rmi web:3.3.1
+docker build -t web:3.3.2 .
+docker save -o web.tar web:3.3.2
+docker rmi web:3.3.2
