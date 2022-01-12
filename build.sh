f80e72624a046cf8a63204834ff30fb53e062e1c
coolwhite520 <81929154@qq.com>
2022-01-12 18:04:14 +0800

    新增图片预览功能

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yarn build
 #read -p "Enter Version:" version
-docker build -t web:3.3.3 .
-docker save -o web.tar web:3.3.3
-docker rmi web:3.3.3
+docker build -t web:3.3.4 .
+docker save -o web.tar web:3.3.4
+docker rmi web:3.3.4
