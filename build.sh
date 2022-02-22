45b9359a7d1c8b1e52dbe4e2a2fac92de3ba696b
coolwhite520 <81929154@qq.com>
2022-02-22 13:10:38 +0800

    add version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yarn build
 #read -p "Enter Version:" version
-docker build -t web:3.3.4 .
-docker save -o web.tar web:3.3.4
-docker rmi web:3.3.4
+docker build -t web:3.3.5 .
+docker save -o web.tar web:3.3.5
+docker rmi web:3.3.5
