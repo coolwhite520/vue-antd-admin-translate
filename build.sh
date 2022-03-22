194b1ef471a5f7e7bdbade99cd8c573f9fe809a2
coolwhite520 <81929154@qq.com>
2022-03-22 17:45:59 +0800

    提升版本

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yarn build
 #read -p "Enter Version:" version
-docker build -t web:3.3.7 .
-docker save -o web.tar web:3.3.7
-docker rmi web:3.3.7
+docker build -t web:3.3.8 .
+docker save -o web.tar web:3.3.8
+docker rmi web:3.3.8
